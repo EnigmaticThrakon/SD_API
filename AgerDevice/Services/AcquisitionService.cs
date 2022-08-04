0db071e77894171003835d8ce6051a3c400b3539
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-03 21:31:36 -0400

    Converted to an IHosted Service

--- AgerDevice/Services/AcquisitionService.cs
+++ AgerDevice/Services/AcquisitionService.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AgerDevice.Services
 {
-    public class AcquisitionService
+    public class AcquisitionService : IHostedService
     {
         public AcquisitionService()
         {
 
         }
+
+        public async Task StartAsync(CancellationToken cancellationToken) { }
+
+        public async Task StopAsync(CancellationToken cancellationToken) { }
     }
 }
