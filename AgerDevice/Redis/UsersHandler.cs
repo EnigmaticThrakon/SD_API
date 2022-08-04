c66ab500219a0b4ca67ac4897b502ef7f2aeea2d
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-04 03:31:19 +0000

    Updated handler to update database if the selected username doesn't already exist

--- AgerDevice/Redis/UsersHandler.cs
+++ AgerDevice/Redis/UsersHandler.cs
@@ -11,17 +11,34 @@ namespace AgerDevice.Redis
     public class UsersHandler
     {
         private readonly ConnectionMultiplexer _redis;
-        private readonly string _tableName;
+        private readonly UserDataKeys _userDataKeys;
 
         public UsersHandler(ConnectionMultiplexer connectionMultiplexer)
         {
             _redis = connectionMultiplexer;
-            _tableName = "users";
+            _userDataKeys = new UserDataKeys();
         }
 
-        public async Task<bool> UsernameExists(string username)
+        public async Task<Guid> ApplyUsername(string username)
         {
-            return true;
+            IDatabase database = _redis.GetDatabase();
+            RedisValue[] results = await database.ListRangeAsync(_userDataKeys.TableName + ':' + _userDataKeys.UsernameList);
+
+            if(results.Any(t => t.ToString() == username)){
+                return Guid.Empty;
+            }
+                
+            Guid userId = Guid.NewGuid();
+            await database.ListLeftPushAsync(_userDataKeys.TableName + ':' + _userDataKeys.UsernameList, username);
+            await database.ListLeftPushAsync(_userDataKeys.TableName + ':' + _userDataKeys.UserIdList, userId.ToString());
+
+            await database.StringSetAsync(_userDataKeys.TableName + ':' + username, userId.ToString());
+
+            string primaryUserKey = _userDataKeys.TableName + ':' + userId.ToString() + ':';
+            await database.StringSetAsync(primaryUserKey + _userDataKeys.Username, username);
+            await database.StringSetAsync(primaryUserKey + _userDataKeys.IsDeleted, "false");
+            await database.StringSetAsync(primaryUserKey + _userDataKeys.LastModified, DateTime.Now.ToString());
+            return userId;
         }
     }
 }
