e9f32d708e5db4137f891557c4d51aa535fd7d60
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-03 21:31:59 -0400

    Added dependency resolutions for the redis handlers and services

---
+++ AgerDevice/DependencyResolution/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgerDevice.Services;
+using AgerDevice.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AgerDevice.DependencyResolution
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAgerDevice(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddAgerDeviceRedisHandlers()
+                .AddAgerDeviceServices();
+        }
+
+        public static IServiceCollection AddAgerDeviceRedisHandlers(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<UsersHandler>();
+            serviceCollection.AddSingleton<SettingsHandler>();
+            serviceCollection.AddSingleton<UnitsHandler>();
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddAgerDeviceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IHostedService, AcquisitionService>();
+            serviceCollection.AddSingleton<AcquisitionService>(t => t.GetServices<IHostedService>().Where(x => x is AcquisitionService).Cast<AcquisitionService>().SingleOrDefault());
+
+            return serviceCollection;
+        }
+    }
+}
