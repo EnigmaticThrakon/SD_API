4303e6dfaa0d6da6a19b3050e6b8f4d759829934
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-04 00:46:30 +0000

    Started adding initial query function

--- AgerDevice/Redis/UsersHandler.cs
+++ AgerDevice/Redis/UsersHandler.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StackExchange.Redis;
+using AgerDevice.Core.Query;
 
 namespace AgerDevice.Redis
 {
     public class UsersHandler
     {
         private readonly ConnectionMultiplexer _redis;
+        private readonly string _tableName;
 
         public UsersHandler(ConnectionMultiplexer connectionMultiplexer)
         {
             _redis = connectionMultiplexer;
+            _tableName = "Users";
+        }
+
+        public List<string> QueryUsers(UsersQuery query)
+        {
         }
     }
 }
