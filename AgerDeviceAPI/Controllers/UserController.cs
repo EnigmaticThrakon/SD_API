a425c40729c325ef4bdcb901b2a84f11be7e8b83
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-04 00:46:44 +0000

    Added users handler to controller

--- AgerDeviceAPI/Controllers/UserController.cs
+++ AgerDeviceAPI/Controllers/UserController.cs
@@ -8,12 +8,16 @@ namespace AgerDeviceAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private ILogger _logger;
+        private readonly ILogger _logger;
+        private readonly UsersHandler _usersHandler;
 
-        public UsersController(ILogger<UsersController> logger)
+        public UsersController(ILogger<UsersController> logger, UsersHandler usersHandler)
         {
             _logger = logger;
+            _usersHandler = usersHandler;
         }
+
+        ///
 
         [HttpGet]
         [Route("{username}")]
