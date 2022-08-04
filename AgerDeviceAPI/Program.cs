8e8ab2a1858fc1583ce395d37017113c67ead85e
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-03 22:20:38 -0400

    Modified for dependency injection and better configuration control

--- AgerDeviceAPI/Program.cs
+++ AgerDeviceAPI/Program.cs
@@ -1,23 +1,82 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace AgerDeviceAPI
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        private static IConfigurationRoot GetConfigurationSettings()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static async Task Main(string[] args)
+        {
+            IConfigurationRoot config = GetConfigurationSettings();
+            IHost apiHost = CreateHostBuilder(config, args).Build();
+            await apiHost.RunAsync();
+        }
+
+        public static void OpenBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(IConfigurationRoot config, string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddCommandLine(args);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(config["server:urls"]);
+                });
+    }
 }
 
-app.UseAuthorization();
+//var builder = WebApplication.CreateBuilder(args);
 
-app.MapControllers();
+//// Add services to the container.
 
-app.Run();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseAuthorization();
+
+////app.MapControllers();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//    endpoints.MapHub<AgerDevice.Hubs.DeviceHub>("/deviceHub");
+//    endpoints.MapHub<AgerDevice.Hubs.MonitorHub>("/monitorHub");
+//});
+
+//app.Run();
---
+++ AgerDeviceAPI/Startup.cs
@@ -0,0 +1,91 @@
+﻿using StackExchange.Redis;
+using AgerDevice.DependencyResolution;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+namespace AgerDeviceAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ConnectionMultiplexer>(t => ConnectionMultiplexer.Connect("localhost"));
+            services.AddAgerDevice();
+
+            services.AddSignalR();
+            services.AddLogging(c =>
+            {
+                c.AddConsole();
+                c.AddDebug();
+            });
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AgerDevice API", Version = "v1" });
+            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AgerDevice API", Version = "v1" });
+
+            //    //c.DocumentFilter<LowercaseDocumentFilter>();
+            //    c.OperationFilter<SecurityRequirementsOperationFilter>();
+
+            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    {
+            //        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+            //        Name = "Authorization",
+            //        In = ParameterLocation.Header,
+            //        Type = SecuritySchemeType.ApiKey
+            //    });
+
+            //    var apiXmlPath = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+            //    //var libraryXmlPath = Path.Combine(AppContext.BaseDirectory, $"VetGuardian.xml");
+
+            //    c.IncludeXmlComments(apiXmlPath);
+            //    //c.IncludeXmlComments(libraryXmlPath);
+            //});
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ServeUnknownFileTypes = true,
+                DefaultContentType = "text/plain"
+            });
+
+            app.UseCors(builder =>
+                builder.SetIsOriginAllowed(_ => true)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            );
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<AgerDevice.Hubs.DeviceHub>("/deviceHub");
+                endpoints.MapHub<AgerDevice.Hubs.MonitorHub>("/monitorHub");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AgerDevice API");
+            });
+        }
+    }
+}
