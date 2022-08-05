44c8f2b5732ad77399e1fee87d5cf4fc0a8d5378
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-04 23:06:54 -0400

    Added test function and removed dependencies that are causing errors

--- AgerDevice/Hubs/DeviceHub.cs
+++ AgerDevice/Hubs/DeviceHub.cs
@@ -12,14 +12,14 @@ namespace AgerDevice.Hubs
     public class DeviceHub : Hub
     {
         private readonly AcquisitionService _acquisitionService;
-        private readonly UserManager _userManager;
-        private readonly UnitManager _unitManager;
+        // private readonly UserManager _userManager;
+        // private readonly UnitManager _unitManager;
 
-        public DeviceHub(AcquisitionService acquisitionService, UserManager userManager, UnitManager unitManager)
+        public DeviceHub(AcquisitionService acquisitionService)//, UserManager userManager, UnitManager unitManager)
         {
             _acquisitionService = acquisitionService;
-            _userManager = userManager;
-            _unitManager = unitManager;
+            // _userManager = userManager;
+            // _unitManager = unitManager;
         }
 
         public async override Task OnConnectedAsync()
@@ -35,6 +35,10 @@ namespace AgerDevice.Hubs
         public async Task SendMessage(string connectionId, string message)
         {
             await Clients.Client(connectionId).SendAsync("SendSignal", Context.ConnectionId, message);
+        }
+
+        public async Task test(string message) {
+            return;
         }
     }
 }
