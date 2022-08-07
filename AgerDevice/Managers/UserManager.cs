212f8436a61d0dc665d2035f49cfc19955f2b3f3
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-07 01:52:43 -0400

    Added the remaining helpful functions to the manager that have to do with the repository

--- AgerDevice/Managers/UserManager.cs
+++ AgerDevice/Managers/UserManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AgerDevice.Core.Repositories;
 using AgerDevice.Core.Models;
+using AgerDevice.Core.Query;
 
 namespace AgerDevice.Managers
 {
@@ -20,6 +21,27 @@ namespace AgerDevice.Managers
         public async Task CreateAsync(User user)
         {
             await _userRepository.CreateAsync(user);
+        }
+
+        public async Task<PagedResult<User>> QueryAsync(UserQuery query = null)
+        {
+            return await _userRepository.QueryAsync(query);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteAsync(Guid userId)
+        {
+            await _userRepository.DeleteAsync(userId);
+        }
+
+        public async Task MarkDeleted(User user)
+        {
+            user.IsDeleted = true;
+            await _userRepository.UpdateAsync(user);
         }
     }
 }
