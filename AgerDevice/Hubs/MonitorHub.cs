b37740a4073883909f5047a4e560da2fa34469dd
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-07 16:16:55 -0400

    Added OnConnectedAsync override to test when a device connects

--- AgerDevice/Hubs/MonitorHub.cs
+++ AgerDevice/Hubs/MonitorHub.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SignalR;
 
 namespace AgerDevice.Hubs
 {
     public class MonitorHub : Hub
     {
+
+        public async override Task OnConnectedAsync()
+        {
+            var feature = Context.Features.Get<IHttpConnectionFeature>();
+            var remoteAddress = feature.RemoteIpAddress;
+            await base.OnConnectedAsync();
+        }
+        
         public async Task JoinGroup(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
