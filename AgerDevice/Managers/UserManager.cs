f753de5418b70e31c08af79fa0b319c875d084b5
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-07 00:18:16 -0400

    Added functions to actually call the creation of user records

--- AgerDevice/Managers/UserManager.cs
+++ AgerDevice/Managers/UserManager.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgerDevice.Core.Repositories;
+using AgerDevice.Core.Models;
 
 namespace AgerDevice.Managers
 {
     public class UserManager
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task CreateAsync(User user)
+        {
+            await _userRepository.CreateAsync(user);
+        }
     }
 }
