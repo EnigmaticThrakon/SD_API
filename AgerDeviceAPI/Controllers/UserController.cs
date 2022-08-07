8b5485246dacc6c8f3a0c0e08625432f8059c244
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-07 16:17:09 -0400

    Changed name of endpoint

--- AgerDeviceAPI/Controllers/UserController.cs
+++ AgerDeviceAPI/Controllers/UserController.cs
@@ -20,13 +20,13 @@ namespace AgerDeviceAPI.Controllers
             _userManager = userManager;
         }
 
-        /// <summary>
+                /// <summary>
         /// Endpoint to get a user ID from a passed in device ID
         /// </summary>
         /// <param name="deviceId"></param>
         /// <returns></returns>
         [HttpPut]
-        [Route("{deviceId}")]
+        [Route("Connect")]
         public async Task<ActionResult<string>> GetUserId(UserViewModel model)
         {
             PagedResult<User> results = await _userManager.QueryAsync(new UserQuery() { DeviceId = model.DeviceId });
@@ -52,5 +52,38 @@ namespace AgerDeviceAPI.Controllers
 
             return newUser.Id.ToString();
         }
+
+        // /// <summary>
+        // /// Endpoint to get a user ID from a passed in device ID
+        // /// </summary>
+        // /// <param name="deviceId"></param>
+        // /// <returns></returns>
+        // [HttpPut]
+        // [Route("{deviceId}")]
+        // public async Task<ActionResult<string>> GetUserId(UserViewModel model)
+        // {
+        //     PagedResult<User> results = await _userManager.QueryAsync(new UserQuery() { DeviceId = model.DeviceId });
+
+        //     if(results.FilteredCount > 0)
+        //     {
+        //         User returningUser = results[0];
+        //         returningUser.LastConnected = DateTime.Now;
+
+        //         await _userManager.UpdateAsync(returningUser);
+        //         return returningUser.Id.ToString();
+        //     }
+
+        //     User newUser = new User() {
+        //         Id = Guid.NewGuid(),
+        //         Modified = DateTime.Now,
+        //         IsDeleted = false,
+        //         DeviceId = model.DeviceId,
+        //         LastConnected = DateTime.Now
+        //     };
+
+        //     await _userManager.CreateAsync(newUser);
+
+        //     return newUser.Id.ToString();
+        // }
     }
 }
