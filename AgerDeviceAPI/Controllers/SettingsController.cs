6d3562e6d6a08e7724fcf130f7ce195edfc9035b
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-09 21:37:55 -0400

    Added endpoints for updating Group Id, getting Group members, and updating User Name

--- AgerDeviceAPI/Controllers/SettingsController.cs
+++ AgerDeviceAPI/Controllers/SettingsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AgerDevice.Core.Models;
+using AgerDevice.Core.Query;
+using AgerDevice.Core.ViewModels;
+using AgerDevice.Managers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgerDeviceAPI.Controllers
@@ -8,21 +12,99 @@ namespace AgerDeviceAPI.Controllers
     public class SettingsController : ControllerBase
     {
         private ILogger _logger;
+        private UserManager _userManager;
 
-        public SettingsController(ILogger<SettingsController> logger)
+        public SettingsController(ILogger<SettingsController> logger, UserManager userManager)
         {
             _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpPut]
+        [Route("${id}/${userName}")]
+        public async Task<ActionResult<string>> UpdateUserName(Guid id, string userName)
+        {
+            try
+            {
+                PagedResult<User> result = await _userManager.QueryAsync(new UserQuery() { IsDeleted = false, Id = id });
+
+                if(result.FilteredCount > 0)
+                {
+                    User currentUser = result[0];
+                    currentUser.UserName = userName.Length > 100 ? userName.Substring(0, 100) : userName;
+                    currentUser.Modified = DateTime.Now;
+
+                    await _userManager.UpdateAsync(currentUser);
+                    return Ok("success");
+                }
+
+                return BadRequest("No User Found With That Id");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
         }
 
         /// <summary>
-        /// Test API Endpoint
+        /// Endpoint to get all members apart of group
         /// </summary>
+        /// <param name="groupId"></param>
         /// <returns></returns>
         [HttpGet]
-        [Route("/test")]
-        public ActionResult Get()
+        [Route("group/members/${groupId}")]
+        public async Task<ActionResult<string[]>> GetGroupMembers(Guid groupId)
         {
-            return Ok();
+            try
+            {
+                PagedResult<User> result = await _userManager.QueryAsync(new UserQuery() { IsDeleted = false, GroupId = groupId });
+
+                if(result.FilteredCount > 0) 
+                {
+                    return Ok(result.Select(t => String.IsNullOrEmpty(t.UserName) ? t.Id.ToString() : t.UserName).ToArray());
+                }
+
+                return new string[0];
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Endpoint to alter users group id
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("group")]
+        public async Task<ActionResult<string>> UpdateUserGroup(GroupViewModel model)
+        {
+            if (model.UserId != null && model.GroupId != null)
+            {
+                try
+                {
+                    PagedResult<User> result = await _userManager.QueryAsync(new UserQuery() { IsDeleted = false, Id = model.UserId });
+
+                    if (result.FilteredCount > 0)
+                    {
+                        User currentUser = result[0];
+                        currentUser.GroupId = model.GroupId.Value;
+                        currentUser.Modified = DateTime.Now;
+
+                        await _userManager.UpdateAsync(currentUser);
+                        return "success";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+
+            }
+
+            return BadRequest("Invalid User or Group ID");
         }
     }
 }
