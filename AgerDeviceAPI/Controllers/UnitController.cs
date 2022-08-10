c08a467e02d83c4b618ee0a128e1c3a5cd063298
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-09 22:09:52 -0400

    Added endpoint to automatically return available units at users public IP

--- AgerDeviceAPI/Controllers/UnitController.cs
+++ AgerDeviceAPI/Controllers/UnitController.cs
@@ -13,14 +13,66 @@ namespace AgerDeviceAPI.Controllers
     {
         private readonly ILogger _logger;
         private readonly UnitManager _unitManager;
+        private readonly UserManager _userManager;
 
-        public UnitController(ILogger<UsersController> logger, UnitManager unitManager)
+        public UnitController(ILogger<UsersController> logger, UnitManager unitManager, UserManager userManager)
         {
             _logger = logger;
             _unitManager = unitManager;
+            _userManager = userManager;
         }
 
-                        /// <summary>
+        /// <summary>
+        /// Endpoint to automatically add units to user based on public IP address
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("NewUnit/Auto")]
+        public async Task<ActionResult<string[]>> AutoAddUnit(GroupViewModel model)
+        {
+            if(model.UserId == null)
+                return BadRequest("User ID Cannot Be Null");
+
+            try
+            {
+            PagedResult<User> userResults = await _userManager.QueryAsync(new UserQuery() { IsDeleted = false, Id = model.UserId });
+
+            User currentUser;
+            if(userResults.FilteredCount > 0)
+            {
+                currentUser = userResults[0];
+            }
+            else
+            {
+                return BadRequest("No User With That ID Found");
+            }
+
+            if(currentUser.PublicIP == null)
+            {
+                return BadRequest("Public IP is Null");
+            }
+
+            PagedResult<Unit> unitResults = await _unitManager.QueryAsync(new UnitQuery() { IsDeleted = false, PublicIP = currentUser.PublicIP });
+
+            if(unitResults.FilteredCount > 0)
+            {
+                //Need to add logic to filter out units already associated with current user group id and returns remaining available units with that IP
+            }
+            else
+            {
+                return new string[0];
+            }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+            return BadRequest("Problem If We Get to Here");
+        }
+
+        /// <summary>
         /// Endpoint to get a user ID from a passed in device ID
         /// </summary>
         /// <param name="deviceId"></param>
