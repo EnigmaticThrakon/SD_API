330398ff671c76f29f89dd69117e1d3213e09c3d
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-09 22:19:57 -0400

    Updated to match Device Hub in getting Public IP

--- AgerDevice/Hubs/MonitorHub.cs
+++ AgerDevice/Hubs/MonitorHub.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgerDevice.Core.Models;
+using AgerDevice.Core.Query;
+using AgerDevice.Managers;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SignalR;
 
@@ -10,11 +13,43 @@ namespace AgerDevice.Hubs
 {
     public class MonitorHub : Hub
     {
+        private readonly UserManager _userManager;
+
+        public MonitorHub(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
 
         public async override Task OnConnectedAsync()
         {
-            var feature = Context.Features.Get<IHttpConnectionFeature>();
-            var remoteAddress = feature.RemoteIpAddress;
+            User currentUser;
+            try 
+            {
+                Guid userId = Guid.Parse(Context.GetHttpContext().Request.Query["user"]);
+                PagedResult<User> result = await _userManager.QueryAsync(new UserQuery() { Id = userId });
+
+                if(result.FilteredCount > 0) 
+                {
+                    currentUser = result[0];
+                }
+                else
+                {
+                    throw new Exception("No Units Found with ID: " + userId);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+                throw new Exception();
+            }
+
+            IHttpConnectionFeature feature = Context.Features.Get<IHttpConnectionFeature>();
+            currentUser.PublicIP = feature.RemoteIpAddress.ToString().Trim();
+            currentUser.LastConnected = DateTime.Now;
+            currentUser.Modified = DateTime.Now;
+
+            await _userManager.UpdateAsync(currentUser);
+
             await base.OnConnectedAsync();
         }
         
