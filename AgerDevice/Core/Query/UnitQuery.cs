ebe8fbc6ff779aa85885a0ef98c9e90d2f2e04f7
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-09 20:18:58 -0400

    Added Unit query

---
+++ AgerDevice/Core/Query/UnitQuery.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AgerDevice.Core.Query
+{
+    public class UnitQuery : Query<UnitQuery.SortColumns>
+    {
+        public Guid? Id { get; set; }
+        public DateTime? Modified { get; set; }
+        public string? SerialNumber { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public UnitQuery()
+        {
+            Id = null;
+            Modified = null;
+            SerialNumber = null;
+            IsDeleted = null;
+        }
+
+        public enum SortColumns
+        {
+            Id,
+            Modified
+        }
+    }
+}
