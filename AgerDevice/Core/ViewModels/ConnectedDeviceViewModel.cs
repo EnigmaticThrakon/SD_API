3ba65c57a5dc6dbf8a48f47585dd622c5975cf6e
EngimaticThrakon <gbrown92799@gmail.com>
2022-08-20 01:01:16 -0400

    Created a from model function for easier transitions

--- AgerDevice/Core/ViewModels/ConnectedDeviceViewModel.cs
+++ AgerDevice/Core/ViewModels/ConnectedDeviceViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgerDevice.Core.Models;
 
 namespace AgerDevice.Core.ViewModels
 {
@@ -16,5 +17,18 @@ namespace AgerDevice.Core.ViewModels
         public string? Name { get; set; }
 
         public ConnectedDeviceViewModel() { }
+
+        static ConnectedDeviceViewModel FromUnitModel(Unit model)
+        {
+            return new ConnectedDeviceViewModel()
+            {
+                Id = model.Id,
+                SerialNumber = model.SerialNumber,
+                PublicIP = model.PublicIP,
+                IsConnected = model.IsConnected,
+                PairedId = model.PairedId,
+                Name = model.Name
+            };
+        }
     }
 }
