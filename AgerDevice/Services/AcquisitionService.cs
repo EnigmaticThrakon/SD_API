7d83f61166ab746f76ed483fb6a33603d7c23378
EngimaticThrakon <gbrown92799@gmail.com>
2022-10-30 13:57:49 -0400

    Added functions and storage to hold the DataAcquisitionServices and start/stop acquisitions

--- AgerDevice/Services/AcquisitionService.cs
+++ AgerDevice/Services/AcquisitionService.cs
@@ -9,14 +9,19 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AgerDevice.Services;
+using AgerDevice.Managers;
 
 namespace AgerDevice.Services
 {
     public class AcquisitionService : IHostedService
     {
-        public AcquisitionService()
+        private ConcurrentDictionary<Guid, UnitAcquisitionService> _unitServices;
+        private UnitManager _unitManager;
+        public AcquisitionService(UnitManager unitManager)
         {
-
+            _unitServices = new ConcurrentDictionary<Guid, UnitAcquisitionService>();
+            _unitManager = unitManager;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken) 
@@ -27,6 +32,28 @@ namespace AgerDevice.Services
         public async Task StopAsync(CancellationToken cancellationToken) 
         {
             await Task.Run(() => { Console.WriteLine("Dumb Placeholder"); });
+        }
+
+        public async Task StartAcquisition(Guid unitId)
+        {
+            UnitAcquisitionService? service = null;
+            service = _unitServices.GetOrAdd(unitId, x => new UnitAcquisitionService(_unitManager));
+
+            if(service != null)
+            {
+                await service.StartAcquisition();
+            }
+        }
+
+        public async Task StopAcquisition(Guid unitId)
+        {
+            UnitAcquisitionService? service = null;
+            _unitServices.TryGetValue(unitId, out service);
+
+            if(service != null)
+            {
+                await service.StopAcquisition();
+            }
         }
     }
 }
