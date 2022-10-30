cb56d7a60e809dec99384bb5b1fd66af676a3c6c
EngimaticThrakon <gbrown92799@gmail.com>
2022-10-30 13:58:23 -0400

    Started setting up the service and added a random number generator to mimic data coming in from the device

--- AgerDevice/Services/UnitAcquisitionService.cs
+++ AgerDevice/Services/UnitAcquisitionService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AgerDevice.Managers;
 
 namespace AgerDevice.Services
 {
     public class UnitAcquisitionService
     {
+        private System.Timers.Timer _acquisitionTimer;
+        private UnitManager _unitManager;
+        private Random _rnd;
+
+        public UnitAcquisitionService(UnitManager unitManager) 
+        {
+            _unitManager = unitManager;
+            _rnd = new Random();
+        }
+
+        public async Task StartAcquisition()
+        {
+            _acquisitionTimer = new (interval: 1500);
+            _acquisitionTimer.Elapsed += async (sender, e) => await SendData();
+            _acquisitionTimer.AutoReset = true;
+            _acquisitionTimer.Start();
+        }
+
+        public async Task StopAcquisition()
+        {
+            _acquisitionTimer.Stop();
+            _acquisitionTimer.Enabled = false;
+            _acquisitionTimer.Dispose();
+        }
+
+        private async Task SendData()
+        {
+            await _unitManager.NewData(_rnd.Next(400));
+        }
     }
 }
