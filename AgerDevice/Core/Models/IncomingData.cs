cc2e5914e2295a291c29de609ac4a939eb53f50a
EngimaticThrakon <gbrown92799@gmail.com>
2022-11-01 00:24:25 -0400

    Added new model to hold data coming from device

---
+++ AgerDevice/Core/Models/IncomingData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgerDevice.Core.Models
+{
+    public class IncomingData
+    {
+        public DateTime? Timestamp { get; set; }
+        public double? Temperature { get; set; }
+        public double? Humidity { get; set; }
+        public double? AirFlow { get; set; }
+        public double? Weight { get; set; }
+        public int? Door { get; set; }
+
+        public IncomingData() { }
+    }
+}
